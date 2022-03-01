87d299958480278d2b3c02024d713a2dedbaff28
diandianyezi <1092923594@qq.com>
2022-03-01 20:24:22 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-usm13q
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-usm13q)
---
+++ src/router/index.ts
@@ -0,0 +1,27 @@
+import {
+  createWebHashHistory,
+  createRouter,
+  Router,
+  RouterOptions,
+  RouteRecordRaw,
+} from 'vue-router';
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    name: 'Index',
+    component: () => import('../views/Index.vue'),
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('../views/Login.vue'),
+  },
+];
+
+const options: RouterOptions = {
+  history: createWebHashHistory(),
+  routes,
+};
+
+const router: Router = createRouter(options);
+export default router;
